72a1d31bcd861abc20d26f2c8c79a7f60f8974e0
JinTae_Park <dktkd7044@gmail.com>
2021-05-27 23:34:07 +0900

    서블릿 DB연동 실습 -Statement 와 PreparedStatement 비교 실습

---
+++ pro07a/src/sec01/ex02/MemberServlet.java
@@ -0,0 +1,60 @@
+package sec01.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberServlet
+ */
+@WebServlet("/member2")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		List<MemberVO> list = new ArrayList<>();
+		MemberDAO dao = new MemberDAO();
+		list = dao.listMembers();
+		
+		out.print("<html><body>");
+		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
+		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
+		
+		for(int i = 0; i < list.size(); i++) {
+			String id = list.get(i).getId();
+			String pwd = list.get(i).getPwd();
+			String name = list.get(i).getName();
+			String email = list.get(i).getEmail();
+			Date joinDate = list.get(i).getJoinDate();
+			
+			out.print("<tr><td>"+id+"</td><td>"+pwd+"</td><td>"+name+"</td><td>"+email+"</td><td>"+joinDate+"</td></tr>");			
+		}
+		out.print("</table></body></html>");
+		out.close();
+	}
+	}
+
+
