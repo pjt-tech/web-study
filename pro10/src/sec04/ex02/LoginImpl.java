223295231f707f5662eb2e713de2a4874ca7e179
JinTae_Park <dktkd7044@gmail.com>
2021-06-05 22:16:21 +0900

    필터와 리스너 기능을 이용한 실습 세션을이용하여 사용자 접속수 표시와 접속한 사용자 아이디 웹사이트에 표시하는 예제 실습

---
+++ pro10/src/sec04/ex02/LoginImpl.java
@@ -0,0 +1,42 @@
+package sec04.ex02;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class LoginImpl
+ *
+ */
+@WebListener
+public class LoginImpl implements HttpSessionListener {
+	
+	String user_id;
+	String user_pw;
+	static int total_user=0;
+	
+    public LoginImpl() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public LoginImpl(String id , String pw) {
+    	this.user_id = id;
+    	this.user_pw = pw;
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent se)  { 
+         System.out.println("技记积己");
+         ++total_user;
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent se)  { 
+    	System.out.println("技记辆丰");
+    	--total_user;
+    }	
+}
