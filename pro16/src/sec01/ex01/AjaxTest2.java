62bf7600296ae8e66ee35e20e580a1d7d4b2f6fd
JinTae_Park <dktkd7044@gmail.com>
2021-06-29 23:56:41 +0900

    jquery ajax통신 실습 xml  형식 에러 부분 확인중

--- pro16/src/sec01/ex01/AjaxTest2.java
+++ pro16/src/sec01/ex01/AjaxTest2.java
@@ -1,6 +1,7 @@
 package sec01.ex01;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -27,16 +28,27 @@ public class AjaxTest2 extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doHandle(request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		doHandle(request,response);
 	}
-
+	
+	private void doHandle (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/xml; charset=utf-8");
+		String result="";
+		PrintWriter writer = response.getWriter();
+		result="<main><book>"+
+		"<title><![CDATA[초보자를 위한 자바 프로그래밍]]></title>" +
+		"<writer><![CDATA[인포북스 저 | 박진태]]</writer>"+
+		"<image><![CDATA[http://localhost:8090/pro16/image/image1.jpg]]></image>" +
+		"</book></main>";
+		System.out.println(result);
+		writer.print(result);
+	}
 }
